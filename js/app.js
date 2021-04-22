2ef6b780bbb1a9e961220848b40edede031f74a8
AniketPithadia <76560880+AniketPithadia@users.noreply.github.com>
2021-04-22 18:06:26 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -4,7 +4,21 @@ $(document).ready(function(){
       slidesToShow:3,
       slidesToScroll:1,
       prevArrow:'.prev-btn',
-      nextArrow:'.next-btn'
+      nextArrow:'.next-btn',
+      responsive:[
+        {
+           breakpoint:992,
+           settings:{
+            slidesToShow:2,
+           }
+        },
+        {
+         breakpoint:786,
+         settings:{
+          slidesToShow:1,
+         }
+        }
+      ]
    });
    
    $('.nav-trigger').click(function(){
